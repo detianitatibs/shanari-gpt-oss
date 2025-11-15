fabb4cc46434ca0b2110aa4cd4ea56fb58f81f81
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-11-15 17:16:24 +0900

    レイアウトを修正

--- web/src/app/profile/layout.tsx
+++ web/src/app/profile/layout.tsx
@@ -1,1 +1,5 @@
 export const layout = 'profileLayout';
+
+export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+  return <div className="profile-layout">{children}</div>;
+}
